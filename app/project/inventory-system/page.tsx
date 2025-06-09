815e5e32da1fc4a9fc997496fdb7969c57dcccf7
satnam2609 <satnamm143@gmail.com>
2025-06-09 21:40:27 +0530

    Typo Correction

--- app/project/inventory-system/page.tsx
+++ app/project/inventory-system/page.tsx
@@ -4,7 +4,7 @@ export default function Project() {
   return (
     <ProjectPage
       title="Inventory System"
-      image="/project/inventory.png"
+      image="/project/Inventory.png"
       description="The Inventory Management System I developed is a full-featured application designed to streamline business operations by combining inventory control with user role management, authentication, and real-time analytics. It enables authenticated users to manage products, track stock levels, and monitor inventory movements while providing role-based access to sensitive operations. The system includes a dynamic dashboard that displays key business metrics and analytics, helping users make data-driven decisions. In addition to inventory tracking, it supports invoice generation and purchase order management, making it a practical tool for handling day-to-day business transactions. The overall design emphasizes usability, security, and operational efficiency, making it well-suited for small to mid-sized businesses seeking an integrated inventory and sales management solution."
       stack={["MongoDB","NextJS"]}
       git="https://github.com/satnam2609/inventory_management_system"
